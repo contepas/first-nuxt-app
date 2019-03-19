ff91f8cff1f85635140bcce61d698642ca82aa1a
PaCo <conte.pas@me.com>
2019-03-19 10:37:48 +0100

    BugFix on authentication cookie and nuxtServerInit

--- store/authentication.js
+++ store/authentication.js
@@ -21,7 +21,7 @@ export const actions = {
         // No valid cookie found
       }
     }
-    commit('setAuth', auth)
+    commit('SET_AUTH', auth)
   }
 }
 
--- store/index.js
+++ store/index.js
@@ -1,4 +1,4 @@
-// export const namespaced = true
+const cookieparser = process.server ? require('cookieparser') : undefined
 
 export const state = () => ({
   profile_info: {}
@@ -16,5 +16,17 @@ export const actions = {
       'https://teamtreehouse.com/pasqualeconte.json'
     )
     commit('SET_PROFILE_INFO', data)
+  },
+  nuxtServerInit({ commit }, { req }) {
+    let auth = null
+    if (req.headers.cookie) {
+      const parsed = cookieparser.parse(req.headers.cookie)
+      try {
+        auth = JSON.parse(parsed.auth)
+      } catch (err) {
+        // No valid cookie found
+      }
+    }
+    commit('authentication/SET_AUTH', auth)
   }
 }
